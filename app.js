61aafc41c3d35daea2e25884df582f02035a7f51
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-10-03 20:01:49 -0400

    Installed Mongoose and connection string

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 
@@ -19,4 +20,11 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured!" });
 });
 
-app.listen(8080);
+mongoose
+  .connect('mongodb+srv://comp229_403:Mlvoi7p8XhkqN6Oc@cluster0.qtp7h.mongodb.net/shared-places?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    app.listen(8080);
+  })
+  .catch(err => {
+    console.log(err);
+  });
